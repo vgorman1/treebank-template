5a94336306c200d8999abcf0e496433105d55672
vgorman1 <vgorman1@unl.edu>
2020-12-26 17:22:35 +0000

    Deploying to gh-pages from  @ 5a726019889c98487e5edcfbe2812f12a51eb640 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.0b17da131bff51781e3ffe62c8dd84d5.js"
+  "/treebank-template/precache-manifest.13c6bf5f2a08a50905d89257d3fb4bd1.js"
 );
 
 self.addEventListener('message', (event) => {
